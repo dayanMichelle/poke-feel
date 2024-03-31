32aab59de3bae3fddf3ea7ca19e918cfb829f9f1
jmoraba_bci <jhan.mora@bci.cl>
2024-03-30 21:20:03 -0300

    feat(1-conceptos-basicos-react): añado TODO en README.md

--- 1-conceptos-basicos-react/src/app.tsx
+++ 1-conceptos-basicos-react/src/app.tsx
@@ -4,7 +4,6 @@ import { SimpleCounter } from "./components/SimpleCounter";
 export const App = () => {
   return (
     <main>
-      <h1>app</h1>
       <SimpleCounter />
       <Counter initialValue={10} step={2} />
     </main>
